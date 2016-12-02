21f3df4c859941f01612673fbac7e7d5f6ce1f8a
stofstik <verstegen.daan@gmail.com>
2016-12-02 10:43:23 +0100

    try using recursion

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Holy crap this is more difficult than I thought...
  */
 
-const words = ['aan', 'uit', 'gaan', 'fruit', 'banaan', 'buskruit', 'verbaast', 'kerst', 'gerst', 'worst', 'borst', 'bosje', 'klosje', 'worstje', 'borstje']
+const words = ['aan', 'gaan', 'uit', 'fruit', 'banaan', 'buskruit', 'verbaast', 'kerst', 'gerst', 'worst', 'borst', 'bosje', 'klosje', 'worstje', 'borstje']
 
 const vowels = [ 'a', 'e', 'i', 'o', 'u', 'y' ]
 const consonants = [ 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z' ]
@@ -21,21 +21,44 @@ function areRiming(word1, word2) {
   // Words are the same
   if(word1 === word2) return true
   // Words are the same after first vowel
-  if(word1.substring(indexOfFirstVowel(word1), word1.length) === word2.substring(indexOfFirstVowel(word2), word2.length)) return true
+  if(word1.substring(indexOfFirstVowel(word1), word1.length) ===
+     word2.substring(indexOfFirstVowel(word2), word2.length)) return true
+
+  return false
 }
 
-let word1 = words[12]
-let word2 = words[11]
-console.log('%s en %s %s', word1, word2, areRiming(word1, word2))
+function recursive(payload) {
+  const { word1, word2 } = payload
+  console.log(payload)
+  if(word1.charAt(word1.length - 1) === word2.charAt(word2.length - 1)) {
+    return recursive({
+        word1: word1.split('').splice(0, word1.length - 1).join(''),
+        word2: word2.split('').splice(0, word2.length - 1).join('')
+    })
+  }
+}
 
-word1 = words[0]
-word2 = words[2]
-console.log('%s en %s %s', word1, word2, areRiming(word1, word2))
-
-word1 = words[0]
-word2 = words[4]
-console.log('%s en %s %s', word1, word2, areRiming(word1, word2))
-
-word1 = words[13]
-word2 = words[14]
-console.log('%s en %s %s', word1, word2, areRiming(word1, word2))
+recursive( { word1: words[11], word2: words[12] } )
+/*
+ *
+ * let word1 = words[0]
+ * let word2 = words[1]
+ * console.log('%s en %s %s', word1, word2, areRiming(word1, word2))
+ *
+ * word1 = words[2]
+ * word2 = words[3]
+ * console.log('%s en %s %s', word1, word2, areRiming(word1, word2))
+ *
+ * word1 = words[0]
+ * word2 = words[4]
+ * console.log('%s en %s %s', word1, word2, areRiming(word1, word2))
+ *
+ * word1 = words[11]
+ * word2 = words[12]
+ * console.log('%s en %s %s', word1, word2, areRiming(word1, word2))
+ *
+ *
+ * word1 = words[13]
+ * word2 = words[14]
+ * console.log('%s en %s %s', word1, word2, areRiming(word1, word2))
+ */
